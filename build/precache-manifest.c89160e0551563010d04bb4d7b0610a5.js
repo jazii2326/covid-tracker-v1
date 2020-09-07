0ecd91a9212ca6889e5c09606d70791bfddada41
Muhammad Ejaz <mijaz9165@gmail.com>
2020-09-07 20:51:05 +0500

    adding files for ghpages

---
+++ build/precache-manifest.c89160e0551563010d04bb4d7b0610a5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8cafad8ae9b2e7dad6084c4ffa761e",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "38c5e1fe42ece2e8859e",
+    "url": "/covid-tracker/static/css/2.16fbd34a.chunk.css"
+  },
+  {
+    "revision": "d90a565c586e9d803105",
+    "url": "/covid-tracker/static/css/main.ee3a3e3d.chunk.css"
+  },
+  {
+    "revision": "38c5e1fe42ece2e8859e",
+    "url": "/covid-tracker/static/js/2.903048ad.chunk.js"
+  },
+  {
+    "revision": "8b00f6fb2d7bb5647faed8d9c17cf2b8",
+    "url": "/covid-tracker/static/js/2.903048ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d90a565c586e9d803105",
+    "url": "/covid-tracker/static/js/main.c115052d.chunk.js"
+  },
+  {
+    "revision": "772efe5590ad79986fbf3a1db778be8b",
+    "url": "/covid-tracker/static/js/main.c115052d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0a0521da445d32754e8",
+    "url": "/covid-tracker/static/js/runtime-main.c2ad502c.js"
+  },
+  {
+    "revision": "01798bc13e33afc36a52f2826638d386",
+    "url": "/covid-tracker/static/media/Pe-icon-7-stroke.01798bc1.ttf"
+  },
+  {
+    "revision": "71394c0c7ad6c1e7d5c77e8ac292fba5",
+    "url": "/covid-tracker/static/media/Pe-icon-7-stroke.71394c0c.eot"
+  },
+  {
+    "revision": "b38ef310874bdd008ac14ef3db939032",
+    "url": "/covid-tracker/static/media/Pe-icon-7-stroke.b38ef310.woff"
+  },
+  {
+    "revision": "dedf26989fdd78c33cb9ae664a604d6c",
+    "url": "/covid-tracker/static/media/Pe-icon-7-stroke.dedf2698.svg"
+  },
+  {
+    "revision": "626d3a5634d67e17eec4ce45e0bd0861",
+    "url": "/covid-tracker/static/media/chemistry.626d3a56.png"
+  },
+  {
+    "revision": "ce7a6b79aa55041f7ae36f6ce22231fe",
+    "url": "/covid-tracker/static/media/face-3.ce7a6b79.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/covid-tracker/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/covid-tracker/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/covid-tracker/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/covid-tracker/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/covid-tracker/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "d6882a51496f88e5f2299b7466d997c4",
+    "url": "/covid-tracker/static/media/patient.d6882a51.png"
+  },
+  {
+    "revision": "f173f966a6731242ca7c641046154a60",
+    "url": "/covid-tracker/static/media/reactlogo.f173f966.png"
+  },
+  {
+    "revision": "0107dd9e02b7d79fabcdc6c32f565e06",
+    "url": "/covid-tracker/static/media/sidebar-3.0107dd9e.jpg"
+  },
+  {
+    "revision": "3c47a5d716f184ce0f3f3b856fd97717",
+    "url": "/covid-tracker/static/media/toxic.3c47a5d7.png"
+  },
+  {
+    "revision": "13a9fbc1e9188a7a7a47b168ff6c5820",
+    "url": "/covid-tracker/static/media/virus.13a9fbc1.png"
+  },
+  {
+    "revision": "0fb8226220a50980431ea29330bbe3ef",
+    "url": "/covid-tracker/static/media/virus2.0fb82262.png"
+  }
+]);
